9bed9b61894a9dcd59119eb9eb7f71bdd57a1ac7
Kevin Ribeiro-Ansoult <Kevin.ribeiro-ansoult@edu.devinci.fr>
2019-10-03 15:21:21 +0200

    feat: calcul vitesse moyenne.

---
+++ Assets/GraphVitesseMoyenne.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphVitesseMoyenne : MonoBehaviour
+{
+    public Chien chien = new Chien("a", 50, 50, 50, 50, 50);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawGraphic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject dot;
+    public GameObject stock;
+
+    void DrawGraphic()
+    {
+        GetComponent<LineRenderer>().widrhg;
+
+        /*GameObject Graph = this.gameObject;
+        // Todo: Ajouter les fonctions qui permette de draw dans un UI le graphique de l'evolution de la vitesse sur le temps
+        for (int i = 0; i <= 185; i++)
+        {
+            var Doto = Instantiate(dot, new Vector3(-70 + i,chien.Acceleration,1), transform.rotation);
+            Doto.transform.SetParent(stock.transform,false);
+        }*/
+        
+    }
+}
--- Assets/Scripts/Chien.cs
+++ Assets/Scripts/Chien.cs
@@ -1,4 +1,6 @@
-﻿[System.Serializable]
+﻿using UnityEngine;
+
+[System.Serializable]
 public class Chien {
     public string Nom;
 
@@ -15,6 +17,8 @@ public class Chien {
     public float Mental;
     
     public float VitesseMoyenne;
+    public float VitesseMoyenneDeuxTournant;
+    public float VitesseMoyenneTroisTournant;
 
     public Chien(string nom,
         float vitesseMaxDefault,
@@ -35,19 +39,123 @@ public class Chien {
         AccelerationDefault = accelerationDefault;
         Mental = mentalDefault;
         MentalDefault = mentalDefault;
+
+        // Calcul vitesse / temps
         CalculVitesseMoyenne();
     }
 
     public void CalculVitesseMoyenne()
     {
+        float distance = 320;
+        float EnduranceConso = 3;
+        float EndurancePourc = 0.75f;
 
-        float aire1 = (VitesseMax * (VitesseMax / Acceleration)) / 2f;
-        float aire2 = ((VitesseMax / ((1- Endurance / 100) * Acceleration)) * VitesseMax) / 2f;
-        float integrale = aire1 + aire2;
+        float TempsAcceleration = VitesseMax / Acceleration;
+        float VitesseAccelerationMoy =
+            (1 / (TempsAcceleration))
+            * VitesseMax
+            * Mathf.Sqrt(Acceleration / VitesseMax)
+            * (2 / 3) * Mathf.Pow(TempsAcceleration, 1.5f);
 
-        float intervalle = (VitesseMax / ((1 - Endurance / 100) * Acceleration)) + (VitesseMax / Acceleration);
+        float TempsEndurance = Endurance / EnduranceConso;
+        float b = (1 / (EnduranceConso * EnduranceConso * TempsEndurance)) - (1 / TempsEndurance);
+        float VitesseEnduMoy =
+            (VitesseMax / TempsEndurance)
+            * ((2 / b) * (Mathf.Sqrt((b * TempsEndurance) + 1) - 1));
 
-        VitesseMoyenne = (integrale * (1 / intervalle));
+        if (distance <= (VitesseAccelerationMoy * TempsAcceleration) + (VitesseEnduMoy * TempsEndurance))
+        {
+            float distanceRaccourci = distance - (VitesseAccelerationMoy * TempsAcceleration);
+            VitesseMoyenne = distance / ( TempsAcceleration
+                + (-b + Mathf.Sqrt(Mathf.Pow(b, 2) + ((4 * Mathf.Pow(VitesseMax, 2)) / Mathf.Pow(distanceRaccourci, 2)))) /
+                (2 * (Mathf.Pow(VitesseMax, 2) / Mathf.Pow(distanceRaccourci, 2))));
+        }
+        else
+        {
+            VitesseMoyenne =
+                distance / 
+                (TempsAcceleration
+                + TempsEndurance
+                + ((distance - (VitesseAccelerationMoy * TempsAcceleration) - (VitesseEnduMoy * TempsEndurance)) * (VitesseMax * EndurancePourc)));
+        }
+
+
+    }
+
+    public void CalculVitesseMoyenneDeuxTournant()
+    {
+        float distance = 525;
+        float EnduranceConso = 3;
+        float EndurancePourc = 0.75f;
+
+        float TempsAcceleration = VitesseMax / Acceleration;
+        float VitesseAccelerationMoy =
+            (1 / (TempsAcceleration))
+            * VitesseMax
+            * Mathf.Sqrt(Acceleration / VitesseMax)
+            * (2 / 3) * Mathf.Pow(TempsAcceleration, 1.5f);
+
+        float TempsEndurance = Endurance / EnduranceConso;
+        float b = (1 / (EnduranceConso * EnduranceConso * TempsEndurance)) - (1 / TempsEndurance);
+        float VitesseEnduMoy =
+            (VitesseMax / TempsEndurance)
+            * ((2 / b) * (Mathf.Sqrt((b * TempsEndurance) + 1) - 1));
+
+        if (distance <= (VitesseAccelerationMoy * TempsAcceleration) + (VitesseEnduMoy * TempsEndurance))
+        {
+            float distanceRaccourci = distance - (VitesseAccelerationMoy * TempsAcceleration);
+
+            VitesseMoyenneDeuxTournant = distance / (TempsAcceleration
+                + (-b + Mathf.Sqrt(Mathf.Pow(b, 2) + ((4 * Mathf.Pow(VitesseMax, 2)) / Mathf.Pow(distanceRaccourci, 2)))) /
+                (2 * (Mathf.Pow(VitesseMax, 2) / Mathf.Pow(distanceRaccourci, 2))));
+        }
+        else
+        {
+            VitesseMoyenneDeuxTournant =
+                distance /
+                (TempsAcceleration
+                + TempsEndurance
+                + ((distance - (VitesseAccelerationMoy * TempsAcceleration) - (VitesseEnduMoy * TempsEndurance)) * (VitesseMax * EndurancePourc)));
+        }
+
+
+    }
+
+    public void CalculVitesseMoyenneTroisTournant()
+    {
+        float distance = 685;
+        float EnduranceConso = 3;
+        float EndurancePourc = 0.75f;
+
+        float TempsAcceleration = VitesseMax / Acceleration;
+        float VitesseAccelerationMoy =
+            (1 / (TempsAcceleration))
+            * VitesseMax
+            * Mathf.Sqrt(Acceleration / VitesseMax)
+            * (2 / 3) * Mathf.Pow(TempsAcceleration, 1.5f);
+
+        float TempsEndurance = Endurance / EnduranceConso;
+        float b = (1 / (EnduranceConso * EnduranceConso * TempsEndurance)) - (1 / TempsEndurance);
+        float VitesseEnduMoy =
+            (VitesseMax / TempsEndurance)
+            * ((2 / b) * (Mathf.Sqrt((b * TempsEndurance) + 1) - 1));
+
+        if (distance <= (VitesseAccelerationMoy * TempsAcceleration) + (VitesseEnduMoy * TempsEndurance))
+        {
+            float distanceRaccourci = distance - (VitesseAccelerationMoy * TempsAcceleration);
+            VitesseMoyenneTroisTournant = distance / (TempsAcceleration
+                + (-b + Mathf.Sqrt(Mathf.Pow(b, 2) + ((4 * Mathf.Pow(VitesseMax, 2)) / Mathf.Pow(distanceRaccourci, 2)))) /
+                (2 * (Mathf.Pow(VitesseMax, 2) / Mathf.Pow(distanceRaccourci, 2))));
+        }
+        else
+        {
+            VitesseMoyenneTroisTournant =
+                distance /
+                (TempsAcceleration
+                + TempsEndurance
+                + ((distance - (VitesseAccelerationMoy * TempsAcceleration) - (VitesseEnduMoy * TempsEndurance)) * (VitesseMax * EndurancePourc)));
+        }
+
 
     }
 
