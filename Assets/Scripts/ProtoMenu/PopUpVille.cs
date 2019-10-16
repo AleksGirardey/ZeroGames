1506dade1107ee6e05f9fd69ab26595904cf9540
Kevin Ribeiro-Ansoult <Kevin.ribeiro-ansoult@edu.devinci.fr>
2019-10-16 13:13:28 +0200

    Added Scenes for menu
    Added Scene for the first menu
    Added Scene for main menus

---
+++ Assets/Scripts/ProtoMenu/PopUpVille.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpVille : MonoBehaviour
+{
+    public Material mat1;
+    public Material mat2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat1 = GetComponent<MeshRenderer>().materials[1];
+        mat2 = GetComponent<MeshRenderer>().materials[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<MeshRenderer>().material = mat1;
+        transform.localScale = new Vector3(transform.localScale.x * 1.02f, transform.localScale.y, transform.localScale.z * 1.02f);
+    }
+
+    private void OnMouseOver()
+    {
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            
+        }*/
+        
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = new Vector3(transform.localScale.x / 1.02f, transform.localScale.y, transform.localScale.z / 1.02f);
+        GetComponent<MeshRenderer>().material = mat2;
+    }
+}
---
+++ Assets/Scripts/ProtoMenu/Ville_PopUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ville_PopUp : MonoBehaviour
+{
+    public Material mat1;
+    public Material mat2;
+
+    public GameObject PopUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat1 = GetComponent<MeshRenderer>().materials[1];
+        mat2 = GetComponent<MeshRenderer>().materials[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<MeshRenderer>().material = mat1;        
+    }
+
+    private void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            PopUp.transform.position = new Vector3(transform.position.x, 0 , transform.position.z + 1.45f);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<MeshRenderer>().material = mat2;
+        
+    }
+}
