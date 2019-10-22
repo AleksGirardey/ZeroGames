73465b71ee33d5d4bb8e97103382b08325ef76e1
AleksGirardey <alexis.girardey@gmail.com>
2019-10-22 18:43:56 +0200

    feat: Ajout DogMovement.cs

---
+++ Assets/Scripts/Objects/DogMovement.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMovement : MonoBehaviour
+{
+    [SerializeField]
+    public Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector3 dogPosition;
+
+    public float speedModifier;
+
+    private bool coroutineAllowed;
+
+    private Rigidbody DogPhysics;
+
+    float TimeSpent;
+
+    private StatsChien StatsChien;
+
+    public float Endurance;
+    public float Acceleration;
+    public float VitesseMax;
+    public float Vitesse;
+    private bool EnduranceConsumed;
+
+    public RaceManager RaceManager;
+
+    void Start()
+    {
+
+        DogPhysics = GetComponent<Rigidbody>();
+        routeToGo = 0;
+        tParam = 0f;
+        speedModifier = 1f;
+        coroutineAllowed = true;
+        Vitesse = 0;
+
+    }
+
+    void Update()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine(GoByTheRoute(routeToGo));
+        }
+
+        if (RaceManager.RaceStarted) SpeedCalc();
+    }
+
+    public void SpeedCalc()
+    {
+        if (Vitesse < VitesseMax && !EnduranceConsumed)
+        {
+            PhaseAcceleration();
+        }
+        else
+        {
+            PhaseDeceleration();
+        }
+        Vitesse = Mathf.Clamp(Vitesse, 6, VitesseMax);
+
+    }
+
+    void PhaseAcceleration()
+    {
+        Vitesse += Time.deltaTime * Acceleration;
+    }
+
+    void PhaseDeceleration()
+    {
+        EnduranceConsumed = true;
+        Vitesse -= Time.deltaTime * Acceleration * ((100 - Endurance) / 100);
+    }
+
+    private IEnumerator GoByTheRoute(int routeNumber)
+    {
+        coroutineAllowed = false;
+        Vector3 p0 = routes[routeNumber].GetChild(0).position;
+        Vector3 p1 = routes[routeNumber].GetChild(1).position;
+        Vector3 p2 = routes[routeNumber].GetChild(2).position;
+        Vector3 p3 = routes[routeNumber].GetChild(3).position;
+
+            while (tParam < 1)
+            {
+                if ((dogPosition - transform.position).magnitude < speedModifier * 3f) // CHECK SI LE CHIEN A VRAIMENT ATTEINT LE POINT
+                {
+                tParam += Time.fixedDeltaTime * speedModifier; // SI OUI, ON AUGMENTE TPARAM DONT DOGPOSITION CALCULE LE NEXT POINT
+                }
+
+                var dir = dogPosition - transform.position;
+
+                // CALCUL DU NEXT POINT
+                dogPosition = Mathf.Pow(1 - tParam, 3) * p0 +
+                3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 +
+                3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 +
+                Mathf.Pow(tParam, 3) * p3;
+
+                // AJOUT DE LA FORCE AU CHIEN
+                DogPhysics.AddForce(dir * Vitesse);
+                
+                // Affichage visuelle du next point (debug)
+                // Debug.DrawLine(transform.position, dogPosition, Color.red, 0.1f);
+
+                // Rotation du chien
+                transform.rotation = Quaternion.LookRotation(Vector3.up, dir) * Quaternion.Euler(90, 0, 0);
+
+                yield return new WaitForEndOfFrame();
+            }
+        tParam = 0f;
+
+        // SWITCH DE BEZIER CURVE
+        routeToGo += 1;
+        if (routeToGo > routes.Length - 1)
+            routeToGo = 0;
+
+        coroutineAllowed = true;
+
+    }
+
+}
