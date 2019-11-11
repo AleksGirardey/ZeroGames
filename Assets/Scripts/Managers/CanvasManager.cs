30b4e584d91d57f23f3850515ca10d7db8103f27
LouisChrr <louis.charrieau@edu.devinci.fr>
2019-11-11 19:43:38 +0100

    feat: UI main, esc, starter menu

---
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject BaseInfos;
+    public GameObject StarterSelectionGO;
+    public GameObject SaveLoadScreen;
+    public GameObject MenuEsc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+
+            MenuEsc.SetActive(!MenuEsc.activeSelf);
+            // A rajouter:  Time.timeScale = 0;
+
+        }
+    }
+
+    public void Save()
+    {
+        print("lol");
+    }
+
+    public void MenuLoad()
+    {
+        SaveLoadScreen.SetActive(!SaveLoadScreen.activeSelf);
+        MenuEsc.SetActive(false);
+    }
+
+    public void GoToStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void StarterSelection()
+    {
+        BaseInfos.SetActive(false);
+        StarterSelectionGO.SetActive(true);
+    }
+
+}
---
+++ Assets/TextCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class TextCanvas : MonoBehaviour
+{
+    public bool isLocalized;
+    private LocalizedText _localization;
+    public string key;
+    public string value;
+    private Text txt;
+
+    private void Awake()
+    {
+        txt = GetComponent<Text>();
+
+        if (isLocalized)
+        {
+            _localization = gameObject.AddComponent<LocalizedText>();
+            _localization.key = key;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = value;
+    }
+}
